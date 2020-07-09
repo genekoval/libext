9bd5a52ff1e3d119eaf53b894985667103756877
Gene Koval <genya@nova.local>
2020-07-09 00:10:28 -0400

    Create 'contains' function for containers

---
+++ include/ext/algorithm.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+
+namespace ext {
+    template <
+        typename Container,
+        typename T = typename Container::value_type
+    >
+    auto contains(const Container& container, const T& value) -> bool {
+        return std::find(
+            container.begin(),
+            container.end(),
+            value
+        ) != container.end();
+    }
+}
