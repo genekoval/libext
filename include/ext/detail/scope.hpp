14ca9bfafc7805edf18f05f7f31bb052596b5ff5
Gene Koval <genyaremchuk@protonmail.com>
2023-08-28 22:32:06 -0400

    Add a scope_exit type for running deferred actions

---
+++ include/ext/detail/scope.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <utility>
+
+namespace ext {
+    template <typename EF>
+    class scope_exit {
+        EF ef;
+    public:
+        scope_exit(EF&& ef) : ef(std::forward<EF>(ef)) {}
+
+        ~scope_exit() {
+            ef();
+        }
+    };
+}
